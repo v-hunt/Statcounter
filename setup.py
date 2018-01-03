5ee6795735280774a990d32a6ebbb557ad730a50
VicHunting <VicHunting@yandex.ua>
2018-01-04 00:36:07 +0200

    Update url in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='statcounter',
     version='0.1',
     packages=find_packages(),
-    url='',
+    url='https://github.com/v-hunt/Statcounter',
     license='BSD',
     author='hunting',
     author_email='VicHunting@yandex.ua',
